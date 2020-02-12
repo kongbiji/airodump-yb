5216a5d0f5d3d1eeac4313efb11e4859cf64f083
biji <heapoverflow@kakao.com>
2020-02-12 22:44:08 +0900

    Create airodump-yb.cpp
    not finished

---
+++ airodump-yb.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pcap.h>
+#include "header.h"
+#include "function.h"
+
+void usage(){
+    printf("usage:   airodump-yb <interface>\n");
+    printf("example: airodump-yb mon0\n");
+}
+
+int main(int argc, char * argv[]){
+
+    if(argc < 2){
+        usage();
+        exit(1);
+    }
+
+    // for test, pcap_open_offline
+    // TODO: capture live packet
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* handle = pcap_open_offline(argv[1], errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "couldn't open file %s: %s\n", argv[1], errbuf);
+        return -1;
+    }
+
+    while(handle != NULL){
+        struct pcap_pkthdr* header;
+        const u_char* data;
+
+        int res = pcap_next_ex(handle, &header, &data);
+        if (res == 0) continue;
+        if (res == -1 || res == -2) break;
+
+        int type = check_packet_type(data);
+
+        switch (type){
+        case BEACON_FRAME:
+            save_Beacon_info(data);
+            
+            break;
+        case PROBE_REQUEST:
+
+            break;
+        case PROBE_RESPONSE:
+
+            break;
+        default:
+            break;
+        }
+
+    }
+    return 0;
+
+
+}

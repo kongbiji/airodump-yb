fefbad2b8436186a2a7c9d800e4c9625dc57b297
biji <heapoverflow@kakao.com>
2020-02-19 16:04:53 +0900

    Update function.h

--- function.h
+++ function.h
@@ -13,6 +13,11 @@ map<MAC, Beacon_values>::iterator beacon_iter;
 map<CONV_MAC, Probe_values> probe_map;
 map<CONV_MAC, Probe_values>::iterator probe_iter;
 
+MAC BROADCAST;
+void init(){
+    memcpy(BROADCAST.mac,"\xFF\xFF\xFF\xFF\xFF\xFF", sizeof(BROADCAST.mac));
+}
+
 // check Type/Subtpye
 int check_packet_type(const unsigned char * data){
     radiotap_header * r_header = (radiotap_header *)malloc(sizeof(radiotap_header));
@@ -34,20 +39,22 @@ int check_packet_type(const unsigned char * data){
         default:
             break;
         }
-    }else if(dot11->Frame_Control_Field.Type == 1){
+    }else if(dot11->Frame_Control_Field.Type == 2){
         if(dot11->Frame_Control_Field.Subtype == 8 ){
             RETURN = QOS_DATA;
-        }else if(dot11->Frame_Control_Field.Subtype == 12){
+        }else if(dot11->Frame_Control_Field.Subtype == 12 || dot11->Frame_Control_Field.Subtype == 4){
             RETURN = QOS_NULL;
         }
     }
     return RETURN;
 }
+
 void print_MAC(uint8_t *addr){
     printf("%02X:%02X:%02X:%02X:%02X:%02X",
            addr[0],addr[1],addr[2],addr[3],
             addr[4],addr[5]);
 }
+
 void save_Beacon_info(const unsigned char * data){
     // for map
     MAC bssid;
@@ -69,7 +76,9 @@ void save_Beacon_info(const unsigned char * data){
     unsigned char * find_ssid = (unsigned char *)(data + r_header->h_len + sizeof(Dot11) + 12);
     if( find_ssid[0] == 0 ){ // SSID parameter
         int len = find_ssid[1];
-        memcpy(val.ssid, find_ssid+2, len);
+        if(len != 0){
+            memcpy(val.ssid, find_ssid+2, len);
+        }
     }
     
     // radiotap fields check
@@ -96,6 +105,7 @@ void save_Beacon_info(const unsigned char * data){
 
     beacon_iter = beacon_map.find(bssid);
     if(beacon_iter != beacon_map.end()){  // already saved AP, update info
+        memcpy(val.ssid, beacon_iter->second.ssid, sizeof(val.ssid));
         int num = beacon_iter->second.Beacons;
         beacon_iter->second = val;
         beacon_iter->second.Beacons = num + 1;
@@ -105,6 +115,7 @@ void save_Beacon_info(const unsigned char * data){
 }
 
 void save_Probe_info(const unsigned char * data, int type){
+
     CONV_MAC key;
     Probe_values val;
     memset(val.probe, 0, sizeof(val.probe));
@@ -113,31 +124,35 @@ void save_Probe_info(const unsigned char * data, int type){
     radiotap_header * r_header = (radiotap_header *)malloc(sizeof(radiotap_header));
     Dot11 * dot11 = (Dot11 *)malloc(sizeof(Dot11 *));
 
-    int * r_header_iter;
+    unsigned char * r_header_iter;
     r_header = (radiotap_header *)data;
-    r_header_iter = (int *)(data + sizeof(radiotap_header));
+    r_header_iter = (unsigned char *)(data + sizeof(radiotap_header));
     dot11 = (Dot11 *)(data + r_header->h_len);
 
     // save probe
-    unsigned char * find_probe = (unsigned char *)(data + r_header->h_len + sizeof(Dot11) + 12);
-    if( find_probe[0] == 0 ){ // SSID parameter
-        int len = find_probe[1];
-        memcpy(val.probe, find_probe+2, len);
-    }
-
+    int type_num;
+    memcpy(key.bssid, dot11->mac3, sizeof(dot11->mac3)); //set bssid
     switch (type){
     case 1:  // Probe request
-        memcpy(key.bssid,"(not associated)", 16); // set bssid
+        type_num = 0;
         memcpy(key.station, dot11->mac2, sizeof(dot11->mac2)); // set station
-        /* code */
         break;
     case 2:  // Probe response
-        memcpy(key.bssid, dot11->mac3, sizeof(dot11->mac3)); //set bssid
+        type_num = 12;
         memcpy(key.station, dot11->mac1, sizeof(dot11->mac1)); // set station
         break;
     default:
         break;
     }
+
+    unsigned char * find_probe = (unsigned char *)(data + r_header->h_len + sizeof(Dot11) + type_num);
+    if( (int)find_probe[0] == 0 ){ // SSID parameter
+        int len = (int)find_probe[1];
+        if(len != 0){
+            memcpy(val.probe, find_probe+2, len);
+        }
+    }
+    
     // radiotap fields check
     if( r_header->presnt_flags.tsft == 1 ){
         r_header_iter = r_header_iter + sizeof(uint64_t);
@@ -149,23 +164,24 @@ void save_Probe_info(const unsigned char * data, int type){
         r_header_iter = r_header_iter + sizeof(uint8_t);
     }
     if( r_header->presnt_flags.channel == 1 ){
-        val.CH = ((int)(r_header_iter[0] | (r_header_iter[1] << 8)) - 2412) / 5 + 1;
         r_header_iter = r_header_iter + sizeof(uint32_t);
     }
     if( r_header->presnt_flags.fhss == 1 ){
         r_header_iter = r_header_iter + sizeof(uint8_t);
     }
     if( r_header->presnt_flags.dbm_antenna_sig == 1 ){
+        
         val.PWR = ((int)r_header_iter[0]-1)^0xFF;
         r_header_iter = r_header_iter + sizeof(uint8_t);
     }
 
     probe_iter = probe_map.find(key);
     if(probe_iter != probe_map.end()){  // already saved AP, update info
+        memcpy(val.probe, probe_iter->second.probe, sizeof(val.probe));
         int num = probe_iter->second.Frames;
         probe_iter->second = val;
         probe_iter->second.Frames = num + 1;
-    }else{ 
+    }else{
         probe_map.insert(pair<CONV_MAC, Probe_values>(key, val));
     }
 }
@@ -179,12 +195,12 @@ void save_QoS_info(const unsigned char * data, int type){
     radiotap_header * r_header = (radiotap_header *)malloc(sizeof(radiotap_header));
     Dot11_data * dot11 = (Dot11_data *)malloc(sizeof(Dot11_data *));
 
-    int * r_header_iter;
+    unsigned char * r_header_iter;
     r_header = (radiotap_header *)data;
-    r_header_iter = (int *)(data + sizeof(radiotap_header));
+    r_header_iter = (unsigned char *)(data + sizeof(radiotap_header));
     dot11 = (Dot11_data *)(data + r_header->h_len);
 
-     switch (type){
+    switch (type){
     case 3:  // QoS Data
         memcpy(key.bssid, dot11->mac2, sizeof(dot11->mac2)); // set bssid
         memcpy(key.station, dot11->mac1, sizeof(dot11->mac1)); // set station
@@ -208,7 +224,6 @@ void save_QoS_info(const unsigned char * data, int type){
         r_header_iter = r_header_iter + sizeof(uint8_t);
     }
     if( r_header->presnt_flags.channel == 1 ){
-        val.CH = ((int)(r_header_iter[0] | (r_header_iter[1] << 8)) - 2412) / 5 + 1;
         r_header_iter = r_header_iter + sizeof(uint32_t);
     }
     if( r_header->presnt_flags.fhss == 1 ){
@@ -218,7 +233,6 @@ void save_QoS_info(const unsigned char * data, int type){
         val.PWR = ((int)r_header_iter[0]-1)^0xFF;
         r_header_iter = r_header_iter + sizeof(uint8_t);
     }
-
 
     probe_iter = probe_map.find(key);
     if(probe_iter != probe_map.end()){  // already saved AP, update info
@@ -233,22 +247,20 @@ void save_QoS_info(const unsigned char * data, int type){
 
 void show_airodump(){
     system("clear");
-    printf("BSSID\t\t\tPWR\tBeacons\t   SSID\n");
+    printf("BSSID\t\t\tPWR\tBeacons\t  CH\t SSID\n\n");
     for(beacon_iter = beacon_map.begin();beacon_iter != beacon_map.end();beacon_iter++){
-        printf("%02X:%02X:%02X:%02X:%02X:%02X",
-           beacon_iter->first.mac[0],beacon_iter->first.mac[1],beacon_iter->first.mac[2],beacon_iter->first.mac[3],
-            beacon_iter->first.mac[4],beacon_iter->first.mac[5]);
-        printf("\t-%d\t%d\t   %s\n", beacon_iter->second.PWR, beacon_iter->second.Beacons, beacon_iter->second.ssid);
+        print_MAC((uint8_t *)beacon_iter->first.mac);
+        printf("\t-%d\t%d\t  %d\t %s\n", beacon_iter->second.PWR, beacon_iter->second.Beacons, beacon_iter->second.CH,beacon_iter->second.ssid);
     }
 
-    printf("\n\nBSSID\t\t\t\tSTATION\t\t\tPWR\t\t\tFrames\t\tProbe\n");
+    printf("\n\nBSSID\t\t\tSTATION\t\t\tPWR\t  Frames   Probe\n\n");
     for(probe_iter = probe_map.begin();probe_iter != probe_map.end();probe_iter++){
-        printf("%02X:%02X:%02X:%02X:%02X:%02X",
-           probe_iter->first.bssid[0],probe_iter->first.bssid[1],probe_iter->first.bssid[2],beacon_iter->first.mac[3],
-            probe_iter->first.bssid[4],probe_iter->first.bssid[5]);
-        printf("\t%02X:%02X:%02X:%02X:%02X:%02X",
-           probe_iter->first.station[0],probe_iter->first.station[1],probe_iter->first.station[2],probe_iter->first.station[3],
-            probe_iter->first.station[4],probe_iter->first.station[5]);
-        printf("\t\t-%d\t\t\t%d\t\t%s\n", probe_iter->second.PWR, probe_iter->second.Frames, probe_iter->second.probe);
+        if(memcmp(probe_iter->first.bssid, BROADCAST.mac, sizeof(MAC)) == 0){
+            printf("(not associated)");
+        }else{
+            print_MAC((uint8_t *)probe_iter->first.bssid);
+        }printf("\t");
+        print_MAC((uint8_t *)probe_iter->first.station);
+        printf("\t-%d\t  %d\t   %s\n", probe_iter->second.PWR, probe_iter->second.Frames, probe_iter->second.probe);
     }
 }
